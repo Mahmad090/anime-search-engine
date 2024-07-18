a9096cadaa31c4512a70496b6100929f64a37f0f
Muhammad Ahmad <97440355+Mahmad090@users.noreply.github.com>
2024-07-18 14:04:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return render_template('index.html')
-
+@app.route('/initial_response', methods=['GET'])
+def initial_response():
+    filename = 'initial_response.wav'
+    predefined_text=generate_response("Start",True)
+    text_to_wav(predefined_text, filename)
+    return send_file(filename, mimetype='audio/wav')
 if __name__ == '__main__':
   app.run(port=5000)
